99a1d44321a360d6e10bc66e82899a0b98244c72
Robert Bjornson <rbjornson@gmail.com>
2018-03-17 15:20:09 -0400

    profiling examples

--- Examples/Profiling/bad2.py
+++ Examples/Profiling/bad2.py
@@ -1,8 +1,7 @@
-
 '''
 This function takes a file containing gene annotions, and another file with mapped reads,
 and annotates the reads with the annotations.
-This version does it in a more efficient way.
+This version does it in a very inefficient way.
 '''
 
 from __future__ import print_function
@@ -10,15 +9,18 @@ from __future__ import print_function
 @profile
 def annotate(gf, mf, of):
     ofp=open(of, "w")
-    geneinfo={}
+    genenames=[]
+    geneinfo=[]
     for l in open(gf):
         gn, chrm, strand, start, end = l.strip().split()
-        geneinfo[gn]=[chrm, strand, start, end]
+        genenames.append(gn)
+        geneinfo.append([chrm, strand, start, end])
 
     for l in open(mf):
         e=l.strip().split()
         genename=e[-1]
-        print('\t'.join(e + geneinfo[genename]), file=ofp)
+        idx=genenames.index(genename)
+        print('\t'.join(e + geneinfo[idx]), file=ofp)
 
 if __name__=='__main__':
-    annotate("genes.txt", "mappedreadswithgenes.txt", "goodgenes.out")
+    annotate("genes.txt", "mappedreadswithgenes.txt", "badgenes.out")
