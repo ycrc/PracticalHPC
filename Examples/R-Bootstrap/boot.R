2552395391b36b8cb573b0fa1ff0fb9a9367f1d0
Robert Bjornson <rbjornson@gmail.com>
2018-03-28 10:20:17 -0400

    more

---
+++ Examples/R-Bootstrap/boot.R
@@ -0,0 +1,25 @@
+
+library(boot)
+
+# tries 5 different regression models on data
+volume_estimate <- function(data, indices){
+ d = data[indices, ]
+ H_relationship = lm(d$Volume~d$Height, data = d)
+ H_r_sq = summary(H_relationship)$r.square
+ G_relationship = lm(d$Volume~d$Girth, data = d)
+ G_r_sq = summary(G_relationship)$r.square
+ G_H_ratio = d$Girth / d$Height
+ G_H_relationship = lm(d$Volume~G_H_ratio, data = d)
+ G_H_r_sq = summary(G_H_relationship)$r.square
+ combined_relationship = lm(d$Volume~d$Height + d$Girth, data = d)
+ combined_r_sq = summary(combined_relationship)$r.square
+ combined_2_relationship = lm(d$Volume~d$Height +d$Girth + G_H_ratio, data = d)
+ combined_2_r_sq = summary(combined_2_relationship)$r.square
+ relationships = c(H_r_sq, G_r_sq, G_H_r_sq, combined_r_sq, combined_2_r_sq)
+ return(relationships)
+}
+
+# bootstrap on tree data
+system.time(res<-boot(data=trees, statistic=volume_estimate, R=5000))
+
+print(res)
