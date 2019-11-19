7df330c1cb5dfb3d0bfd7b07390e8f364f7263bf
Robert Dean Bjornson of group lsprog <rdb9@grace1.grace.hpc.yale.internal>
2019-11-19 17:50:35 -0500

    added CaseStudy

---
+++ Examples/CaseStudy/batch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --array 1-22
+#SBATCH -c 10 
+
+module load parallel
+
+c=$SLURM_ARRAY_TASK_ID
+
+n=$SLURM_CPUS_PER_TASK
+
+cp bigfile.$c /tmp
+
+parallel -j $n "sleep 10; ./prog {1} {2} {3} /tmp/bigfile.$c > output.{1}_{2}_{3}_{#}" ::: lung heart liver ::: e1 e2 e3 ::: 0.01 0.05
+
+rm /tmp/bigfile.$c
