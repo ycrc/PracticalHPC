235baecab50cd91cabfa8062616a0c6e10b667b8
Robert Dean Bjornson of group lsprog <rdb9@grace1.grace.hpc.yale.internal>
2019-11-19 18:00:36 -0500

    fixed small bug

--- Examples/CaseStudy/batch.sh
+++ Examples/CaseStudy/batch.sh
@@ -11,6 +11,6 @@ n=$SLURM_CPUS_PER_TASK
 
 cp bigfile.$c /tmp
 
-parallel -j $n "sleep 10; ./prog {1} {2} {3} /tmp/bigfile.$c > output.{1}_{2}_{3}_{#}" ::: lung heart liver ::: e1 e2 e3 ::: 0.01 0.05
+parallel -j $n "sleep 10; ./prog {1} {2} {3} /tmp/bigfile.$c > output.${c}_{1}_{2}_{3}" ::: lung heart liver ::: e1 e2 e3 ::: 0.01 0.05
 
 rm /tmp/bigfile.$c
